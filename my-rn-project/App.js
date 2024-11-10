c6b8da04024352a54d5090f076d6af829458b392
Valen Lambert <vlambert@udesa.edu.ar>
2024-11-10 18:45:44 -0300

    cambios en la base

--- my-rn-project/App.js
+++ my-rn-project/App.js
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View >
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
